2709a8d98f795cdbdd6a2d0c4fb867b2eba9de0d
He Lin <helinchooi@gmail.com>
2022-12-18 12:21:20 +0800

    Shuffle the food card position

--- src/pages/FoodPage.js
+++ src/pages/FoodPage.js
@@ -8,11 +8,29 @@ const FoodPage = () => {
   const { id } = useParams()
   let navigate = useNavigate();
 
+  function shuffle(array) {
+    let currentIndex = array.length, randomIndex;
+  
+    // While there remain elements to shuffle.
+    while (currentIndex !== 0) {
+  
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+  }
+
   return (
     <div className='container'>
       <div className='title'>KK12 Random Food Picker</div>
       <div className='card-container'>
-        <FoodPanel food={food[id]}></FoodPanel>
+        <FoodPanel food={shuffle(food)[id]}></FoodPanel>
       </div>
       <div className='button-container'>
 
